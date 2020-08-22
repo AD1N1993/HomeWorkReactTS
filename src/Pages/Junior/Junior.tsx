61c5d69d047fd47f806d4e21b98d690ce8aee9af
Yury <griv1993@gmail.com>
2020-08-22 19:26:20 +0300

    HomeWork 7 part 2

--- src/Pages/Junior/Junior.tsx
+++ src/Pages/Junior/Junior.tsx
@@ -5,12 +5,24 @@ import {EditAbleSpanLC} from "../../components/EditAbleSpanLC/EditAbleSpan";
 import {Select} from "../../common/Select/Select";
 import {RadioButton} from "../../common/RadioButton/RadioButton";
 
-export type InputSettingsType ={
-    id:number
-    title:string
+export type InputSettingsType = {
+    id: string
+    title: string
+    isDone: boolean
 }
+
 export function Junior() {
+    let radioSettings: Array<InputSettingsType> = [
+        {id: "1", title: "Beer", isDone: false},
+        {id: "2", title: "Milk", isDone: false},
+        {id: "3", title: "Jin", isDone: false},
+        {id: "4", title: "Tequila", isDone: false},
+    ]
+
+    let [radioState, setRadioState] = useState(radioSettings);
+
     let [inputValue1, setInputValue1] = useState<string>("Your name");
+
 
     const onChangeInputValue1 = (newInputValue: string) => {
         setInputValue1(newInputValue)
@@ -22,37 +34,38 @@ export function Junior() {
         setInputValue2(newInputValue)
     }
 
-    let optionsValue = ["Minsk","Moscow","London","Bon"]
+    let optionsValue = ["Minsk", "Moscow", "London", "Bon"]
     let [selectValue, setSelectValue] = useState(optionsValue[0])
 
-    const onChangeSelectOption = (option:string) =>{
+    const onChangeSelectOption = (option: string) => {
         setSelectValue(option);
     }
 
+    const onChangeRadioInputStatus = (radioItemId: string, isDone: boolean) => {
+        console.log(radioItemId, isDone)
+        let clickItem = radioState.find(i => i.id === radioItemId);
+        if (clickItem) {
+            clickItem.isDone = !clickItem.isDone
+            setRadioState([...radioState]);
+        }
 
-    let radioSettings:Array<InputSettingsType> = [
-        {id:1, title: "Beer"},
-        {id:1, title: "Milk"},
-        {id:1, title: "Jin"},
-        {id:1, title: "Tequila"},
-    ]
-
+    }
 
 
     return (
         <div className={s.wrapper}>
 
-                <h2>Home Task 6</h2>
-                <EditAbleSpan value={inputValue1} onChange={onChangeInputValue1}/>
+            <h2>Home Task 6</h2>
+            <EditAbleSpan value={inputValue1} onChange={onChangeInputValue1}/>
 
-                <h4>Home Task 6 with *</h4>
+            <h4>Home Task 6 with *</h4>
 
-                <EditAbleSpanLC value={inputValue2} onChange={onChangeInputValue2}/>
+            <EditAbleSpanLC value={inputValue2} onChange={onChangeInputValue2}/>
 
             <h2>Home Task7</h2>
             <Select value={selectValue} options={optionsValue} onChange={onChangeSelectOption}/>
             <hr/>
-            <RadioButton radioSettings={radioSettings}  name={"Drinks"}  />
+            <RadioButton values={radioState} name={"Drinks"} onChange={onChangeRadioInputStatus}/>
         </div>
     );
 }
--- src/common/RadioButton/RadioButton.tsx
+++ src/common/RadioButton/RadioButton.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, {MouseEvent, useState} from "react";
 import {InputSettingsType} from "../../Pages/Junior/Junior";
+import s from "./RadioButton.module.scss"
 
 
 type RadioButtonPropsType = {
-    radioSettings: Array<InputSettingsType>
+    values: Array<InputSettingsType>
     name: string
-    // onChange: () => void
+    onChange: (radioItemId: string, isDone: boolean) => void
 }
 
 
@@ -13,9 +14,23 @@ export const RadioButton = (props: RadioButtonPropsType) => {
 
     return (
         <>
-            <h3>{props.name}</h3>
-            {props.radioSettings.map(i => {
-                return<><input type="radio" value={i.title} /> <span>{i.title}</span></>
+            <h3 className={s.radioButtonTitle}>{props.name}</h3>
+            {props.values.map(i => {
+                const onChangeRadioButton = (event: MouseEvent<HTMLInputElement>) => {
+                    props.onChange(event.currentTarget.id, event.currentTarget.checked);
+                }
+                return <>
+                    <label className={s.radioItem}>
+                        <input className={s.radioInput}
+                               type="radio"
+                               value={i.title}
+                               onClick={(event) => onChangeRadioButton(event)}
+                               id={i.id}
+                               checked={i.isDone}
+                        />
+                        <span className={`${s.radioItemTitle} ${i.isDone? s.active:""}`}>{i.title}</span>
+                    </label>
+                </>
             })}
         </>
     );
